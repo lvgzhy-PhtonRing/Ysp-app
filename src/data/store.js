d2264b5bfa9a5735574c0b2355372501747bb66a
lvgzhy-phtonring <lvgzhy@gmail.com>
2026-04-18 18:59:43 +0800

    chore: bump app version to 3.0.0

--- src/data/store.js
+++ src/data/store.js
@@ -2,7 +2,7 @@
 
 import { reactive } from 'vue'
 
-const APP_VERSION = '2.1.2'
+const APP_VERSION = '3.0.0'
 const CLOUD_SYNC_DEBOUNCE_MS = 800
 const MAX_UNDO_STEPS = 20
 const HISTORY_META_EXPIRE_MS = 3000
